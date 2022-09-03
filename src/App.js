95eb3ce26f517857038b375a449985b54dbe1ad8
Constanza <comallea@gmail.com>
2022-09-03 19:17:14 -0400

    added protected routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import "./App.scss";
-import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login";
+import HomePage from "./pages/Home";
+//import { ProfilePage } from "./pages/Profile";
+//import { SettingsPage } from "./pages/Settings";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
-function App() {
+export default function App() {
   return (
-    <div className="container">
-      <Login />
-    </div>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+      {/*       <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <ProtectedRoute>
+            <SettingsPage />
+          </ProtectedRoute>
+        }
+      /> */}
+      <Route path="*" element={<LoginPage />} />
+    </Routes>
   );
 }
-
-export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { login } from "../api/LoginApi";
+//import { login } from "../api/LoginApi";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import styles from "./Login.module.scss";
