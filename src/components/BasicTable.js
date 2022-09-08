22b7264cb348549d3cbfbbc646002e264fe1d331
Constanza <comallea@gmail.com>
2022-09-08 03:10:01 -0400

    added loading to products table

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -7,18 +7,9 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { getProductosProductor } from "../api/LoginApi";
 import React, { useEffect, useState } from "react";
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import styles from "./BasicTable.module.scss";
 
 export default function BasicTable(props) {
   const [isError, setIsError] = useState(false);
@@ -55,7 +46,11 @@ export default function BasicTable(props) {
     }
   };
 
-  return (
+  return loading ? (
+    <Box className={styles.loadingContainer}>
+      <CircularProgress />
+    </Box>
+  ) : (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
---
+++ src/components/Formulario.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Formulario = () => {
+  return <div>Formulario</div>;
+};
