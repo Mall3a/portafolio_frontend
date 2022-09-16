7983d04945a1a954ed657bd738436c2c25e63d8a
Constanza <comallea@gmail.com>
2022-09-16 09:51:30 -0300

    added material react table to project

--- src/api/MockData.js
+++ src/api/MockData.js
@@ -29,3 +29,51 @@ export const mockProducts = {
   },
   status: 200,
 };
+
+export const personData = [
+  {
+    name: {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    address: "261 Erdman Ford",
+    city: "East Daphne",
+    state: "Kentucky",
+  },
+  {
+    name: {
+      firstName: "Jane",
+      lastName: "Doe",
+    },
+    address: "769 Dominic Grove",
+    city: "Columbus",
+    state: "Ohio",
+  },
+  {
+    name: {
+      firstName: "Joe",
+      lastName: "Doe",
+    },
+    address: "566 Brakus Inlet",
+    city: "South Linda",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "Kevin",
+      lastName: "Vandy",
+    },
+    address: "722 Emie Stream",
+    city: "Lincoln",
+    state: "Nebraska",
+  },
+  {
+    name: {
+      firstName: "Joshua",
+      lastName: "Rolluffs",
+    },
+    address: "32188 Larkin Turnpike",
+    city: "Omaha",
+    state: "Nebraska",
+  },
+];
--- src/components/driver/Auctions.js
+++ src/components/driver/Auctions.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useMemo } from "react";
+import MaterialReactTable from "material-react-table";
+import { personData } from "../../api/MockData";
 
 const Auctions = () => {
-  return <div>Auctions</div>;
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "name.firstName", //access nested data with dot notation
+        header: "First Name",
+      },
+      {
+        accessorKey: "name.lastName",
+        header: "Last Name",
+      },
+      {
+        accessorKey: "address", //normal accessorKey
+        header: "Address",
+      },
+      {
+        accessorKey: "city",
+        header: "City",
+      },
+      {
+        accessorKey: "state",
+        header: "State",
+      },
+    ],
+    []
+  );
+
+  return <MaterialReactTable columns={columns} data={personData} />;
 };
 
 export default Auctions;
