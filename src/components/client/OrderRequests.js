2d8b2775717359b68232053f04fad730d85260a1
Constanza <comallea@gmail.com>
2022-11-09 22:25:42 -0300

    fixed detail

--- src/components/client/OrderRequests.js
+++ src/components/client/OrderRequests.js
@@ -36,7 +36,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 500,
+  width: 600,
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
@@ -247,8 +247,6 @@ const OrderRequests = ({ user }) => {
                                 <TableCell align="right">
                                   Cantidad&nbsp;(kg)
                                 </TableCell>
-
-                                <TableCell align="right">Eliminar</TableCell>
                               </TableRow>
                             </TableHead>
                             <TableBody>
